26c807be0ea2db294c757d854ef790699aee25ef
unknown <2020CS680@student.uet.edu.pk>
2024-04-26 12:18:32 +0500

    Added Seller Folder

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,11 +1,12 @@
 import Order from '../models/order.js';
+import mongoose from 'mongoose';
 // Order Controller
 const createOrder = async (req, res) => {
     // If user is not logged in, return error
     // Create order
     try {
         // Check if user is logged in
-        const  buyer = req.session.userId;
+        const buyer = req.session.userId;
         const { orderNumber, totalPrice, taxAmount, discount, products, shippingAddress, paymentMethod } = req.body;
         // if (buyer === undefined && buyer || orderNumber === undefined || totalPrice === undefined || taxAmount === undefined || discount === undefined || products === undefined || shippingAddress === undefined || paymentMethod === undefined || status === undefined) {
         //     return res.status(400).json({ message: 'Invalid data' });
@@ -29,16 +30,27 @@ const createOrder = async (req, res) => {
 
 const getOrders = async (req, res) => {
     try {
-        //find all orders and populate buyer and products
-        const orders = await Order.find().populate('buyer').populate('products.product');
-        const totalIncome = orders.reduce((acc, order) => acc + order.totalPrice, 0);
-        console.log(totalIncome);
-        res.status(200).json(orders);
-    }   
-    catch (error) {
+        const sellerId = "6616be959f1d5253c11dd5f9"; // Assuming you get the seller id from the request params
+
+        const orders = await Order.aggregate([
+            // Unwind the products array to access the product documents
+            { $unwind: "$products" },
+            // Lookup the product details using the product id
+            { $lookup: { from: "products", localField: "products.product", foreignField: "_id", as: "product" } },
+            // Unwind the product array to flatten the result
+            { $unwind: "$product" },
+            // Match orders that have products related to the specified seller id
+            { $match: { "product.seller": new mongoose.Types.ObjectId(sellerId) } }
+        ]);
+
+        res.status(200).json({ orders });
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
+
+
+
 
 const trackOrder = async (req, res) => {
     const { orderNumber } = req.body;
@@ -63,21 +75,21 @@ const updateOrderStatus = async (req, res) => {
     //     status
     // })
     switch (status) {
-        case 'cancel':
-            if (order.status == 'complete') {
+        case 'canceled':
+            if (order.status == 'confirmed') {
                 return res.status(400).json({ message: 'Cannot cancel completed order' });
             }
             else {
-                order.status = 'cancel';
+                order.status = 'canceled';
                 await order.save();
                 return res.status(200).json({ message: 'Order cancelled' });
             }
-        case 'complete':
-            if (order.status == 'cancel') {
+        case 'confirmed':
+            if (order.status == 'canceled') {
                 return res.status(400).json({ message: 'Cannot complete cancelled order' });
             }
             else {
-                order.status = 'complete';
+                order.status = 'confirmed';
                 await order.save();
                 return res.status(200).json({ message: 'Order completed' });
             }
